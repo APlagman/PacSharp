2efeff8037555f5bda0a9f889294b770c978a654
Alex Plagman <alexplagman@gmail.com>
2018-03-01 19:28:26 -0800

    Encapsulate power pellet size within object class

--- PacSharp/PacSharpApp/Objects/PowerPellet.cs
+++ PacSharp/PacSharpApp/Objects/PowerPellet.cs
@@ -1,12 +1,11 @@
-﻿using System.Drawing;
-using PacSharpApp.Graphics;
+﻿using PacSharpApp.Graphics;
 
 namespace PacSharpApp.Objects
 {
     class PowerPellet : GameObject
     {
-        internal PowerPellet(Size size, GraphicsHandler handler)
-            : base(size)
+        internal PowerPellet(GraphicsHandler handler)
+            : base(GraphicsHandler.TileSize)
         {
             handler.RegisterAnimatedSprite(this, new PowerPelletSprite() { Palette = PaletteID.Pellet });
         }
