128676ec2f1ac4109d884f7b8bd885cc587d47ae
Alex Plagman <alexplagman@gmail.com>
2018-03-02 14:01:45 -0800

    Add Benchmark class

---
+++ PacSharp/PacSharpApp/Utils/Benchmark.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PacSharpApp.Utils
+{
+    public class Benchmark
+    {
+        private static DateTime startDate = DateTime.MinValue;
+        private static DateTime endDate = DateTime.MinValue;
+
+        public static TimeSpan Span => endDate.Subtract(startDate);
+
+        public static void Start() => startDate = DateTime.Now;
+
+        public static void End() => endDate = DateTime.Now;
+
+        public static double GetSeconds()
+            => (endDate == DateTime.MinValue) ? 0.0 : Span.TotalSeconds;
+    }
+}
