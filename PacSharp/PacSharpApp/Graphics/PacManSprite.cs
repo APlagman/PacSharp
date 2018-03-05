fe59c9da5248b514660326908d48369df0cd826a
Alex Plagman <alexplagman@gmail.com>
2018-03-04 20:35:10 -0800

    Start PacMan's sprite with his mouth closed

--- PacSharp/PacSharpApp/Graphics/PacManSprite.cs
+++ PacSharp/PacSharpApp/Graphics/PacManSprite.cs
@@ -12,9 +12,9 @@ namespace PacSharpApp.Graphics
         private static readonly Bitmap sourceSheet = Resources.Sprites;
         private static readonly (Bitmap, TimeSpan)[] sourceImages = new(Bitmap, TimeSpan)[]
         {
-            (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanOpenRight), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50)),
-            (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanMiddleRight), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50)),
             (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanSolid), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50)),
+            (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanMiddleRight), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50)),
+            (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanOpenRight), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50)),
             (sourceSheet.Clone(GraphicsUtils.GetGraphicLocation(GraphicsID.SpritePacmanMiddleRight), sourceSheet.PixelFormat), TimeSpan.FromMilliseconds(50))
         };
 
